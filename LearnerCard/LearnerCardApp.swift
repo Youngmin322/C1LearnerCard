565e163d3dc3f6125ece7c061b7e345e7c423b62
Youngmin322 <choyoungmin0322@gmail.com>
2026-03-20 14:39:04 +0900

    Initial Commit

---
+++ LearnerCard/LearnerCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnerCardApp.swift
+//  LearnerCard
+//
+//  Created by Youngmin Cho on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct LearnerCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
