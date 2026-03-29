5870b969b37c7e5bac2f863cc142a9825f5ff29d
Youngmin322 <choyoungmin0322@gmail.com>
2026-03-29 15:58:26 +0900

    feat: ConnectionService에 프로토콜 채택

--- LearnerCard/Services/Connection​Service.swift
+++ LearnerCard/Services/Connection​Service.swift
@@ -9,16 +9,54 @@ import SwiftUI
 import NearbyInteraction
 import MultipeerConnectivity
 
-class ConnectionService {
+class ConnectionService: NSObject, MCSessionDelegate, MCNearbyServiceBrowserDelegate, MCNearbyServiceAdvertiserDelegate {
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+                    
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: (any Error)?) {
+        
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+        
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        
+    }
+    
     private let myPeerID: MCPeerID
     private let session: MCSession
     private let nearbyServiceAdvertiser: MCNearbyServiceAdvertiser
     private let nearbyServiceBrowser: MCNearbyServiceBrowser
-        
+    
     init(displayName: String) {
         myPeerID = MCPeerID(displayName: displayName)
         session = MCSession(peer: myPeerID)
         nearbyServiceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerID, discoveryInfo: nil, serviceType: "learner-card")
         nearbyServiceBrowser = MCNearbyServiceBrowser(peer: myPeerID, serviceType: "learner​-card")
+        
+        super.init()
+        
+        session.delegate = self
+        nearbyServiceAdvertiser.delegate = self
+        nearbyServiceBrowser.delegate = self
     }
 }
