562398c4418fc7b8c5857976d6bf4d8f1f516a72
Youngmin322 <choyoungmin0322@gmail.com>
2026-04-14 12:00:49 +0900

    feat: 애플워치 지원을 위한 WatchApp 파일 추가

---
+++ LearnerCard Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnerCard Watch Watch App
+//
+//  Created by Youngmin Cho on 4/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
