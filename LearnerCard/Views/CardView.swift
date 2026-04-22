a815c66ee39d4acf45394fd072db47ae2e049adf
Youngmin322 <choyoungmin0322@gmail.com>
2026-04-22 16:00:01 +0900

    style: 카드 디자인 개선

--- LearnerCard/Views/CardView.swift
+++ LearnerCard/Views/CardView.swift
@@ -11,8 +11,8 @@ struct CardView: View {
     var learnerCard: Card
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            HStack {
+        HStack() {
+            VStack {
                 Circle()
                     .frame(width: 40, height: 40)
                     .overlay(
@@ -21,61 +21,70 @@ struct CardView: View {
                             .bold()
                     )
                     .foregroundColor(.blue)
-                
-                VStack(alignment: .leading) {
+            }
+            VStack(alignment: .leading) {
+                HStack {
+                    VStack(alignment: .leading) {
                     Text(learnerCard.name)
                         .bold()
                     Text(learnerCard.nickName)
                 }
+                    Spacer()
+                    
+                    Text(learnerCard.session)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 4)
+                        .background(Capsule().fill(Color.blue.opacity(0.15)))
+                        .foregroundColor(.blue)
+                        .font(.caption)
+                }
                 
-                Spacer()
+                Divider()
+                    .background(.black)
                 
-                Text(learnerCard.session)
-                    .padding(.horizontal, 10)
-                    .padding(.vertical, 4)
-                    .background(Capsule().fill(Color.blue.opacity(0.15)))
-                    .foregroundColor(.blue)
-                    .font(.caption)
-            }
-            
-            Divider()
-            
-            HStack {
-                Text("전화번호")
-                    .foregroundColor(.gray)
-                    .font(.subheadline)
-                
-                Spacer()
-                
-                Text(learnerCard.phone ?? "없음")
-                    .font(.subheadline)
-            }
-            
-            Divider()
-            
-            VStack(alignment: .leading, spacing: 4) {
-                Text("추가 정보")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                
-                ForEach(learnerCard.descriptions, id: \.self) { description in
-                    Text("\(description)")
+                HStack {
+                    Text("전화번호")
+                        .foregroundColor(.gray)
                         .font(.subheadline)
-                        .padding(.horizontal)
+                    
+                    Spacer()
+                    
+                    Text(learnerCard.phone ?? "없음")
+                        .font(.subheadline)
+                }
+                
+                Divider()
+                    .background(.black)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("추가 정보")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    
+                    ForEach(learnerCard.descriptions, id: \.self) { description in
+                        Text("\(description)")
+                            .font(.subheadline)
+                            .padding(.horizontal)
+                    }
                 }
             }
         }
+        .padding(.trailing)
         .padding()
-        #if os(watchOS)
+#if os(watchOS)
         .frame(width: 190, height: 150)
-        #else
+#else
         .frame(width: 350, height: 190)
-        #endif
+#endif
         .glassEffect(.regular, in: .rect(cornerRadius: 20))
+        .overlay(RoundedRectangle(cornerRadius: 20)
+            .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+        )
         .contentShape(Rectangle())
         .padding()
     }
 }
+
 
 #Preview {
     CardView(learnerCard: Card(name: "조영민", nickName: "Owen", session: "오전", profileImageURL: nil, phone: nil, descriptions: [""], isMine: true))
